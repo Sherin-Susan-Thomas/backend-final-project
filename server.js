d9e81a43758d5f9274ef66fdc6304d0d2c457692
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-06-16 17:51:26 +0200

    added cors

--- server.js
+++ server.js
@@ -61,7 +61,13 @@ const authenticateUser = async (req, res, next) => {
     });
   }
 };
-
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
